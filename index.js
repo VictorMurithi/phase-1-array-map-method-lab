137b459a7a67f3f67787f634167f439aae5039d6
victor <victor.murithi@student.moringaschool.com>
2023-09-26 18:56:56 +0300

    completed assignment

--- index.js
+++ index.js
@@ -10,7 +10,12 @@ const tutorials = [
   'what is the difference between event capturing and bubbling?',
   'what is JSONP?'
 ];
-
-const titleCased = () => {
-  return tutorials
+function titleCased() {
+  return tutorials.map((string) => {
+    const splitWords = string.split(' ');
+    const titleCasedWords = splitWords.map((word) => {
+      return word[0].toUpperCase() + word.slice(1);
+    });
+    return titleCasedWords.join(' ');
+  });
 }
